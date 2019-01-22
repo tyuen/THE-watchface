235a9ef7c2ba69fc12ab304e30ab958694d02a56
Terry Yuen <tyuen1@gmail.com>
2019-01-22 11:13:31 +0800

    Update index.jsx

--- settings/index.jsx
+++ settings/index.jsx
@@ -1,3 +1,17 @@
+function isNotEmpty(list) {
+  list = list && JSON.parse(list);
+  list = list && list.values;
+  return list && (list.length === 1 ? list[0].value !== "none" : list.length > 1);
+}
+
+function getClampedFirstStat(props) {
+  let first = props.settingsStorage.getItem("firstStat2");
+  let stats = props.settingsStorage.getItem("stats");
+  if(first && stats) {
+    return Math.min(JSON.parse(first).selected[0], JSON.parse(stats).selected.length - 1);
+  }
+}
+
 registerSettingsPage(props => {
   return (
     <Page>
@@ -19,21 +33,39 @@ registerSettingsPage(props => {
         />      
       </Section>
 
-      <Section title="Health Rings &amp; Numbers" description="Tap on watch face to cycle through the numbers.">
+      <Section title="Health Rings">
         <Toggle settingsKey="hideRings" label="Hide rings" />
-        <Select label="Default number"
-          settingsKey="firstStat"
+      </Section>  
+
+      <Section title="Health Stats" description="Tap on bottom half of watch face to cycle through the stats.">
+        <Select label="Show Stats"
+          multiple
+          selectViewTitle="Show Stats"
+          settingsKey="stats"
           options={[
-            {name: "None", value: 6},
-            {name: "Steps", value: 0},
-            {name: "Heart Rate (bpm)", value: 1},
-            {name: "Distance (km/mi)", value: 2},
-            {name: "Floors Climbed (f)", value: 3},
-            {name: "Calories Burned (cal)", value: 4},
-            {name: "Active Time (hh'mm'')", value: 5}
+            {name: "None", value: "none"},
+            {name: "Steps", value: "steps"},
+            {name: "Heart Rate (bpm)", value: "heart"},
+            {name: "Resting Heart Rate (- bpm -)", value: "rest"},
+            {name: "Distance (km/mi)", value: "dist"},
+            {name: "Floors Climbed (f)", value: "floors"},
+            {name: "Calories Burned (cal)", value: "cals"},
+            {name: "Active Time (hh'mm'')", value: "mins"},
+            {name: "Weight (kg/lb)", value: "weight"},
+            {name: "Digital Time", value: "time"},
+            {name: "Battery", value: "batt"}
           ]}
-        />
-        <Toggle settingsKey="unboldStats" label="Don't bold numbers" />
+        />        
+
+        {(isNotEmpty(props.settings.stats) ?
+          <Select label="Default"
+            selectViewTitle="Default Stat"
+            options={JSON.parse(props.settings.stats).values}
+            onSelection={sel => props.settingsStorage.setItem("firstStat2", JSON.stringify(sel))}
+            selected={getClampedFirstStat(props)}
+          /> : null)}
+       
+        <Toggle settingsKey="unboldStats" label="Narrow Font" />
       </Section>
     </Page>
   );
