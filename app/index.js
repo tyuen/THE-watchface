598903dda3b484c3bc968dd7b370c859d42e93a7
Terry Yuen <tyuen1@gmail.com>
2019-03-19 19:32:18 +0800

    Update index.js

--- app/index.js
+++ app/index.js
@@ -5,7 +5,9 @@ import {HeartRateSensor} from "heart-rate";
 import {display} from "display";
 import {vibration} from "haptics";
 import {peerSocket} from "messaging";
-import {units} from "user-settings";
+import {preferences, units} from "user-settings";
+import {me} from "appbit";
+import {user} from "user-profile";
 import * as fs from "fs";
 import {battery} from "power";
 import {decode} from "cbor";
@@ -22,30 +24,41 @@ const THEMES = {
   grey: ["888888", "666666", "444444"],
   white: ["FFFFFF", "FFFFFF", "FFFFFF"]
 };
+const HOUR12 = (preferences.clockDisplay === "12h");
+const PROFILE = me.permissions.granted("access_user_profile");
+const NOCLIMB = (health.today.local.elevationGain === undefined);
+
 let weekNames = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
 
-var lastUpdatedRings = 0;
-var lastUpdatedHeart = 0;
-var showRings = true;
-var unboldStats = false;
-var firstStat = 6;  //6=blank
-var curStat = 6;
-var heartSensor;
+let lastUpdatedRings = 0;
+let lastUpdatedHeart = 0;
+let showRings = true;
+let unboldStats = false;
 
-var myDate = $("mydate");
-var myWeek = $("myweek");
-var myHours = $("hours");
-var myMins = $("minutes");
-var mySecs = $("seconds");
-var myStats = $("mystats");
-var myBatt = $("batt");
+let stats = ["none", "steps", "heart", "floors", "cals", "mins", "time"];
+let firstStat = 0;  //0=blank
+let curStat = 0;
+let heartSensor;
+
+let myDate = $("mydate");
+let myWeek = $("myweek");
+let myHours = $("hours");
+let myMins = $("minutes");
+let mySecs = $("seconds");
+let myStats = $("mystats");
+let myBatt = $("batt");
+
+let myRingTL = $("today_tl");
+let myRingTR = $("today_tr");
+let myRingBL = $("today_bl");
+let myRingBR = $("today_br");
 
 function $(s) {
   return document.getElementById(s);
 }
 
-function onTick() {
-  let now = new Date();
+function onTick(now) {
+  now = (now && now.date) || new Date();
   myDate.text = now.getDate();
   myWeek.text = weekNames[now.getDay()];
 
@@ -58,7 +71,7 @@ function onTick() {
 
   myBatt.x2 = Math.round(battery.chargeLevel*7/25) - 14;
 
-  if(showRings || curStat !== 6) {
+  if(showRings || (stats.length > 0 && stats[curStat] !== "none")) {
     let nowTime = now.getTime();
     
     if(showRings) {
@@ -66,15 +79,19 @@ function onTick() {
         lastUpdatedRings = nowTime;
         let today = health.today.adjusted;
         let goal = health.goals;
-        updateHealth("today_tl", "cal", goal, today);
-        updateHealth("today_tr", "step", goal, today);
-        updateHealth("today_br", "dist", goal, today);
-        updateHealth("today_bl", "climb", goal, today);
+        updateRing(myRingTL, "cal", goal, today);
+        updateRing(myRingTR, "step", goal, today);
+        updateRing(myRingBR, "dist", goal, today);
+        if(NOCLIMB) {
+          updateRing(myRingBL, "active", goal, today);
+        } else {
+          updateRing(myRingBL, "climb", goal, today);
+        }
       }
     }
 
-    if(curStat !== 6) {
-      if(curStat !== 1) {
+    if(stats.length > 0 && stats[curStat] !== "none") {
+      if(stats[curStat] !== "heart") {
         updateStat();
       } else {
         if(nowTime - lastUpdatedHeart > 1600) {
@@ -102,15 +119,17 @@ $("top_half").onclick = () => {
 };
 
 $("btm_half").onclick = () => {
-  curStat = (curStat + 1) % 7;
-  if(curStat === 1) {
-    updateHeart();
-  } else {
-    updateStat();
+  if(stats.length > 0) {
+    curStat = (curStat + 1) % stats.length;
+    if(stats[curStat] === "heart") {
+      updateHeart();
+    } else {
+      updateStat();
+    }
   }
 };
 
-function updateHealth(id, holder, goal, today) {
+function updateRing(node, holder, goal, today) {
   let angle = 0;
   if(holder === "cal") {
     angle = (today.calories || 0)*360/(goal.calories || 400);
@@ -123,20 +142,43 @@ function updateHealth(id, holder, goal, today) {
   } else if(holder === "active") {
     angle = (today.activeMinutes || 0)*360/(goal.activeMinutes || 30);
   }
-  $(id).sweepAngle = Math.min(360, Math.round(angle));
+  node.sweepAngle = Math.min(360, Math.round(angle));
 }
 
 function updateStat() {
   let today = health.today.adjusted;
-  switch(curStat) {
-    case 0: myStats.text = today.steps; break;
-    case 1: break;  //heart
-    case 2: myStats.text = (units.distance === "metric") ? round(today.distance/1000) + " km" : round(today.distance/1609.34) + " mi"; break;
-    case 3: myStats.text = today.elevationGain + " f"; break;
-    case 4: myStats.text = today.calories + " cal"; break;
-    case 5:
+  switch(stats[curStat]) {
+    case "steps":
+      myStats.text = today.steps; break;
+    case "heart":
+      break;
+    case "dist": 
+      myStats.text = (units.distance === "metric") ? round(today.distance/1000) + " km" : round(today.distance/1609.34) + " mi";
+      break;
+    case "floors":
+      myStats.text = (today.elevationGain || 0) + " f";
+      break;
+    case "cals":
+      myStats.text = today.calories + " cal";
+      break;
+    case "mins":
       let t = today.activeMinutes;
-      myStats.text = Math.floor(t/60) + "' " + pad(t % 60) + '"'; break;
+      myStats.text = Math.floor(t/60) + "' " + pad(t % 60) + '"';
+      break;
+    case "time":
+      let t = new Date();
+      let hr = t.getHours();
+      myStats.text = ((hr > 12 && HOUR12) ? hr % 12 : hr) + ":" + pad(t.getMinutes());
+      break;
+    case "weight":
+      myStats.text = !PROFILE ? "No Access" : (units.weight === "metric" ? round(user.weight) + " kg" : round(user.weight/2.2046) + " lb");
+      break;
+    case "rest":
+      myStats.text = !PROFILE ? "No Access" : "- " + user.restingHeartRate + " -";
+      break;
+    case "batt":
+      myStats.text = battery.chargeLevel + "%";
+      break;
     default: myStats.text = "";
   }
 }
@@ -159,17 +201,18 @@ function updateHeart() {
   if(!h) {
     heartSensor = h = new HeartRateSensor();
     h.onreading = () => {
-      h.stop();
+      setTimeout(() => h.stop(), 100);
       clearTimeout(delayHeart);
       myStats.text = h.heartRate;
     };
     h.onerror = () => {
-      h.stop();
+      setTimeout(() => h.stop(), 100);
       clearTimeout(delayHeart);
       myStats.text = "--";
     };
   }
   if(!h.activated) {
+    clearTimeout(delayHeart);
     delayHeart = setTimeout(() => {
       myStats.text = "--";
     }, 500);
@@ -186,6 +229,9 @@ function applySettings(o) {
       while(node = nodes[j++]) node.style.fill = "#" + colors[i];
     }
   }
+  if(o.days) {
+    weekNames = o.days;
+  }
   if("hideRings" in o) {
     showRings = !o.hideRings;
     let nodes = document.getElementsByClassName("rings");
@@ -196,44 +242,54 @@ function applySettings(o) {
     unboldStats = o.unboldStats;
     myStats.style.fontFamily = unboldStats ? "System-Regular" : "System-Bold";
   }
-  if("firstStat" in o) curStat = firstStat = o.firstStat;
+  if("stats" in o) stats = o.stats;
+  if("firstStat" in o) curStat = firstStat = Math.min(o.firstStat, stats.length - 1);
   myStats.text = "";
   lastUpdatedRings = 0;
   lastUpdatedHeart = 0;
 }
 
-peerSocket.onmessage = e => {
-  e = e.data;
-  if(e) {
-    applySettings(e);
-    display.poke();
-    vibration.start("bump");
-    fs.writeFileSync("settings2.txt", e, "cbor");
-  }
-};
-
 function parseFile(name) {
   let obj;
   try {
     obj = fs.readFileSync(name, "cbor");
-  } catch(e) {}
+  } catch(e) {
+    return true;
+  }
+
   if(name === "settings2.txt") {
-    if(obj) {
-      applySettings(obj);
-    } else {
-      peerSocket.onopen = () => {peerSocket.send({getAll: 1})};
-    }
-  } else if(name === "days.txt") {
-    if(obj) weekNames = obj.days;
+    if(obj) applySettings(obj);
   }
 }
 
 function pendingFiles() {
+  let found = false;
   let temp;
-  while(temp = inbox.nextFile()) parseFile(temp);
+  while(temp = inbox.nextFile()) {
+    parseFile(temp);
+    found = true;
+  }
+  if(found) {
+    display.poke();
+    vibration.start("bump");
+  }
 }
 
 pendingFiles();
 inbox.onnewfile = pendingFiles;
-parseFile("settings2.txt");
-parseFile("days.txt");
+
+if(parseFile("settings2.txt")) {
+  let done = (peerSocket.readyState === peerSocket.OPEN);
+  if(done) {
+    peerSocket.send({getAll: 1});
+  } else {
+    peerSocket.onopen = () => {
+      if(!done) peerSocket.send({getAll: 1});
+      done = true;
+    };
+  }
+}
+
+if(NOCLIMB) {
+  $("floors").href = "ico_active.png";
+}
