aec4f94edf5b13df2d21c5ddc35d324cab9fcad4
Terry Yuen <tyuen1@gmail.com>
2019-03-19 19:35:00 +0800

    Update index.js

--- companion/index.js
+++ companion/index.js
@@ -14,28 +14,27 @@ peerSocket.onmessage = e => {
 if(me.launchReasons.settingsChanged) sendAll();
 
 function sendAll() {
-  if(peerSocket.readyState === peerSocket.OPEN) {
-    sendAllNow();
-  } else {
-    let done = false;
-    peerSocket.onopen = () => {
-      if(!done) sendAllNow();
-      done = true;
-    };
+  let obj = {
+    theme: trim(store.getItem("theme") || "blue"),
+    hideRings: (store.getItem("hideRings") === "true"),
+    unboldStats: (store.getItem("unboldStats") === "true"),
+    stats: ["none", "steps", "heart", "floors", "cals", "mins", "time"],
+    firstStat: 0,
+    days: getLocale()
+  };
+  if(store.getItem("stats")) {
+    obj.stats = JSON.parse(store.getItem("stats")).values.map(n => n.value);
   }
-  let i18n = getLocale();
-  if(i18n) outbox.enqueue("days.txt", encode(i18n));
-}
-
-function sendAllNow() {
-  if(peerSocket.readyState === peerSocket.OPEN) {
-    peerSocket.send({
-      theme: trim(store.getItem("theme") || "blue"),
-      hideRings: (store.getItem("hideRings") === "true"),
-      unboldStats: (store.getItem("unboldStats") === "true"),
-      firstStat: store.getItem("firstStat") ? JSON.parse(store.getItem("firstStat")).values[0].value : 6
-    });
+  if(store.getItem("firstStat2")) {
+    let value = JSON.parse(store.getItem("firstStat2")).values[0].value;
+    for(let i = 0; i < obj.stats.length; i++) {
+      if(value === obj.stats[i]) {
+        obj.firstStat = i;
+        break;
+      }
+    }
   }
+  outbox.enqueue("settings2.txt", encode(obj));
 }
 
 function trim(s) {
@@ -48,7 +47,9 @@ function getLocale() {
   } catch(e) {
     let lang = locale.language.replace("_", "-");
     let days = [];
-    for(let i = 0; i < 7; i++) days.push(new Date(2000, 0, i + 2).toLocaleDateString(lang, {weekday: "short"}).toUpperCase().replace(".", ""));
-    return {days};
+    for(let i = 0; i < 7; i++) {
+      days.push(new Date(2000, 0, i + 2).toLocaleDateString(lang, {weekday: "short"}).toUpperCase().replace(".", ""));
+    }
+    return days;
   }
 }
